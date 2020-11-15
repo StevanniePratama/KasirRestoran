a4b795d6919f6b750b5fda6bb395aaea25c5429b
Vann30 <stevanniepratama12@gmail.com>
2020-11-15 12:24:12 +0700

    Update Pertama

---
+++ main.py
@@ -0,0 +1,15 @@
+from os import system as syst
+from datetime import datetime
+
+import system
+
+MenuPaketan = system.load_MenuSatu()
+MenuTerbaru = system.load_MenuDua()
+MenuBurger = system.load_MenuTiga()
+MenuAyam = system.load_MenuEmpat()
+MenuNasi = system.load_MenuLima()
+MenuMie = system.load_MenuEnam()
+MenuSnack = system.load_MenuTujuh()
+MenuMinuman = system.load_MenuDelapan()
+MenuSadMeal = system.load_MenuSembilan()
+
